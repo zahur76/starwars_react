92a0b3e7a0919e07ad3d3575f2689a11fe89c13d
zahur <zahurmeerun@hotmail.com>
2022-08-10 17:25:57 +0400

    add: login form to sign in modal

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,21 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faKey } from '@fortawesome/free-solid-svg-icons'
+
 import { useEffect, useState } from "react"
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
 
 
 
 function Header(props) {
 
+    // modal open close
     const [show, setShow] = useState(false);
-
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
@@ -25,18 +32,30 @@ function Header(props) {
             </Row>
 
             <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
+                <Modal.Header className='border-0' closeButton>
+                    <Modal.Title>Sign In</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
+                <Modal.Body>
+                <Form>                    
+                    <InputGroup className="mb-3">
+                    <InputGroup.Text id="basic-addon1"><FontAwesomeIcon icon={faUser} /></InputGroup.Text>
+                    <Form.Control
+                    placeholder="Username or Email"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1" required/>
+                    </InputGroup>                    
+                    <InputGroup className="mb-3">
+                    <InputGroup.Text id="basic-addon1"><FontAwesomeIcon icon={faKey} /></InputGroup.Text>
+                    <Form.Control
+                    placeholder="Password"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1" required/>
+                    </InputGroup>
+                    <Button variant="dark" className="btn-sm" ype="submit">
+                        Submit
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
+                </Form>
+                </Modal.Body>
             </Modal>
         </div>
 
